307461f04eea7ac62997e137357b3cf2f5cd8478
sakio318 <saki1803terashima@gmail.com>
2020-12-14 06:52:53 +0000

    made_view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/new'
+  get 'post/show'
+  get 'post/edit'
   devise_for :users
   root to: 'homes#top'
+
+  resources :user, only: [:index, :show, :edit, :update, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_091748) do
+ActiveRecord::Schema.define(version: 2020_12_14_054236) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "text"
+    t.string "img"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_12_13_091748) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "nam"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
